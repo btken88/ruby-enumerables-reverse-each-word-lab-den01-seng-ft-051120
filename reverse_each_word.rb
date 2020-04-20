a99bbc0fea849b86700d30eb0f14cea043a9d057
Bryce Kennedy <btken88@gmail.com>
2020-04-20 20:20:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,15 @@ def reverse_each_word(string)
   }
   new_string.join(" ")
 end
+
+def reverse_each_word(string)
+  # convert the string into an array of words
+  string_array = string.split(" ")
+  # new array with reversed words
+  new_string = []
+  string_array.each {|word|
+    new_string << word.reverse
+  }
+  # convert new array to string
+  new_string.join(" ")
+end
