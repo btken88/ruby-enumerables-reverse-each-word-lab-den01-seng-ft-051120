9187e5a7a907367f5a975033aa7aa0b29f3edcc3
Bryce Kennedy <btken88@gmail.com>
2020-04-20 20:23:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,21 +2,20 @@ def reverse_each_word(string)
   # convert the string into an array of words
   string_array = string.split(" ")
   # new array with reversed words
-  new_string = []
+  reversed_words= []
   string_array.each {|word|
-    new_string << word.reverse
+    reversed_words << word.reverse
   }
-  new_string.join(" ")
+  # convert reversed words array to string
+  reversed_words.join(" ")
 end
 
 def reverse_each_word(string)
   # convert the string into an array of words
   string_array = string.split(" ")
   # new array with reversed words
-  reversed_words= []
-  string_array.each {|word|
-    reversed_words << word.reverse
+  reversed_words = string_array.collect {|word|
+    word.reverse
   }
-  # convert reversed words array to string
   reversed_words.join(" ")
 end
